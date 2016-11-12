e1ef4c38208e3502dc144a3382fd4a7a16dbf4bf
sasaki-github <harumi20081222papa20071222@gmail.com>
2016-11-12 09:22:12 +0900

    init Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by 佐々木和明 on 2016/11/04.
+//  Copyright © 2016年 佐々木和明. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //タップ時の処理を設定
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func buttonClicked() {
+        let alertCOntroller = UIAlertController(title: "Hello", message: "Clicked", preferredStyle: UIAlertControllerStyle.alert);
+        
+        alertCOntroller.addAction(UIAlertAction(title: "OK!", style:UIAlertActionStyle.default, handler:nil));
+        
+        present(alertCOntroller, animated:true, completion:nil);
+    }
+    
+    @IBAction func exitButtonClicked() {
+        applicationFinishedRestoringState();
+    }
+}
